ae08ab92c54cded076f25a4b79c6c861b7aa9f43
Cotel <miguelcoletomunoz@hotmail.com>
2015-01-20 19:19:05 +0100

    Improved flow on main.py

--- source/main.py
+++ source/main.py
@@ -13,35 +13,37 @@ def main():
 		iv.close()
 
 
-	while True:		
+	while True:
+				
 		print "1. Encriptar contrasena y guardar en BBDD"
 		print "2. Desencriptar contrasena de la BBDD"
 		print "0. Salir"
 		print
-		print "Selecciona una accion: "
+		print "Selecciona una accion: "		
 		seleccion = input("> ")
-
+		
 		if seleccion == 0:
 			break
 		elif seleccion == 1:
 			with open ("iv.dat","r") as myfile:
 				bb=myfile.read()
-			servicio = raw_input("Servicio: ")
-			usuario = raw_input("Usuario del servicio: ")			
-			passwd = raw_input("Contrasena: ")
-			print servicio, usuario, passwd, "es correcto? (y/n) "
-			check = raw_input("> ")
-			if check == "y" or check == "Y" or check == "yes" or check == "Yes" or check == "YES":
-				pass
-			elif check == "n" or check == "N" or check == "No" or check == "no" or check == "NO":
-				print
-				print
-				main()
-			else:
-				print "Introduce una opcion valida"
-				print
-				print
-				main()
+
+			valid_input = False
+			while not valid_input:	
+				servicio = raw_input("Servicio: ")
+				usuario = raw_input("Usuario del servicio: ")			
+				passwd = raw_input("Contrasena: ")
+				print servicio, usuario, passwd, "es correcto? (y/n) "
+			
+				check = raw_input("> ")
+				if check == "y" or check == "Y" or check == "yes" or check == "Yes" or check == "YES":
+					valid_input=True
+				elif check == "n" or check == "N" or check == "No" or check == "no" or check == "NO":
+					print									
+				else:
+					print "Introduce una opcion valida"
+					print
+									
 			passwd1 = Password(usuario,servicio,passwd)
 			print "Encriptando..."
 			print passwd1.encrypt(bb)
@@ -68,6 +70,11 @@ def main():
 					passwd2 = Password(usuario,servicio,passwd[0])
 					passwdf = passwd2.decrypt(bb)
 				print "Contasena: " , passwdf
+			print
+			print
+
+		else:
+			print "Introduce una opcion valida"
 			print
 			print
 
